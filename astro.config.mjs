2a46a8a083bc7c44391c28a693369de9a331abfe
riceball <42161586+riceball-tw@users.noreply.github.com>
2026-06-03 20:40:41 +0800

    chore(astro.config.mjs): Wrong remotePatterns  config

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,12 +35,14 @@ export default defineConfig({
 	base: finalBase,
 	// !IMPORTANT: Set site url property with your own domain
 	site: baseUrl,
-	remotePatterns: [
-		{
-			protocol: "https",
-			hostname: "bucket-webdong.webdong.dev",
-		},
-	],
+	image: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "bucket-webdong.webdong.dev",
+			},
+		],
+	},
 	redirects: {
 		"/": `${finalBase}${defaultLocale}/`,
 		"/post/": `${finalBase}${defaultLocale}/post/`,
